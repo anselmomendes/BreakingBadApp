10558eba012050fe5a0c15af40ac66ea8964fa39
Anselmo <anselmo.96@hotmail.com>
2020-07-27 02:29:07 -0300

    Page Mortes

--- lib/app/modules/deaths/deaths_page.dart
+++ lib/app/modules/deaths/deaths_page.dart
@@ -1,4 +1,7 @@
 import 'package:BreakingBadApp/app/modules/deaths/models/DeathsModel.dart';
+import 'package:BreakingBadApp/app/modules/widgets/app_bar.dart';
+import 'package:BreakingBadApp/app/modules/widgets/card_deaths.dart';
+import 'package:BreakingBadApp/app/modules/widgets/top_container.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_modular/flutter_modular.dart';
@@ -17,98 +20,50 @@ class _DeathsPageState extends ModularState<DeathsPage, DeathsController> {
 
   @override
   Widget build(BuildContext context) {
-    var screenWidth = MediaQuery.of(context).size.width;
+    var width = MediaQuery.of(context).size.width;
+    var height = MediaQuery.of(context).size.height;
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        backgroundColor: Colors.green[700],
-        title: Text(widget.title),
-      ),
-      body: Container(
-        child: Observer(
-          builder: (_) {
-            if (controller.deaths == null)
-              return Center(
-                child: CircularProgressIndicator(),
-              );
+      body: SingleChildScrollView(
+        child: Container(
+          height: height,
+          width: width,
+          color: Colors.green[700],
+          child: Column(
+            children: [
+              CustonAppBar(
+                title: widget.title,
+                height: height,
+                width: width,
+              ),
+              TopContainer(
+                width: width,
+                height: height,
+                child: Observer(
+                  builder: (_) {
+                    if (controller.deaths == null)
+                      return Center(
+                        child: CircularProgressIndicator(),
+                      );
 
-            List<DeathsModel> list = controller.deaths;
-            if (controller.deaths.isEmpty) {
-              return Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Center(child: CircularProgressIndicator()),
-                ],
-              );
-            }
-            return ListView.builder(
-              itemCount: controller.deaths.length,
-              itemBuilder: (_, index) {
-                DeathsModel model = list[index];
-                return Card(
-                  child: Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 15),
-                    child: Container(
-                      width: screenWidth * 0.95,
-                      height: screenWidth * 0.48,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.start,
+                    List<DeathsModel> list = controller.deaths;
+                    if (controller.deaths.isEmpty) {
+                      return Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                          Container(
-                              //color: Colors.green,
-                              color: Colors.green[300],
-                              width: screenWidth * 0.95,
-                              child: Text('  Vítima : ${model.death}',
-                                  overflow: TextOverflow.clip,
-                                  maxLines: 1,
-                                  style: TextStyle(
-                                    fontSize: 18,
-                                    fontWeight: FontWeight.w500,
-                                  ))),
-                          Text('Assassino: ${model.responsible}',
-                              overflow: TextOverflow.clip,
-                              maxLines: 1,
-                              style: TextStyle(
-                                  fontSize: 16, fontWeight: FontWeight.w700)),
-                          Text('Últimas Palavras: ${model.last_words}',
-                              overflow: TextOverflow.clip,
-                              maxLines: 2,
-                              style: TextStyle(
-                                  fontSize: 13, fontWeight: FontWeight.w400)),
-                          Text('Temporada: ${model.season}',
-                              overflow: TextOverflow.clip,
-                              maxLines: 1,
-                              style: TextStyle(
-                                  fontSize: 16, fontWeight: FontWeight.w400)),
-                          Text(
-                            'Episódio: ${model.episode}',
-                            overflow: TextOverflow.clip,
-                            maxLines: 1,
-                            style: TextStyle(
-                                fontSize: 16, fontWeight: FontWeight.w400),
-                          ),
-                          SizedBox(height: 10),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            crossAxisAlignment: CrossAxisAlignment.end,
-                            children: [
-                              Icon(Icons.sentiment_very_dissatisfied),
-                              Text('Homicídios:  '),
-                              SizedBox(
-                                width: 30,
-                              ),
-                              Text(model.number_of_deaths.toString())
-                            ],
-                          ),
+                          Center(child: CircularProgressIndicator()),
                         ],
-                      ),
-                    ),
-                  ),
-                );
-              },
-            );
-          },
+                      );
+                    }
+                    return CardDeaths(
+                      height: height,
+                      width: width,
+                      list: list,
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
